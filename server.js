66ad9edecacb43481b1b4bef505682b5f913a7df
Mateus <correia.mateus@hotmail.com>
2023-12-05 18:19:08 -0300

    Serverjs, database setup, user model and index view

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const express = require("express");
+const session = require("express-session");
+
+const passport = require("passport");
+const path = require("path");
+const db = require("./database/database");
+
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render("Index");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is up!");
+
+    db.then(() => {
+        console.log("DB is up!");
+    })
+    .catch(error => {
+        console.log("Error on DB: " + error);
+    });    
+});
